b81099e37f3680a65fc2166b5309b641f9f70cff
MarcosEzeArg <marcosezequielarganaraz@gmail.com>
2026-08-20 13:54:08 -0300

    update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from './components/Navbar';
-import Home from "./page/Home";
-import Login from "./page/Login";
-import RegistroEmprendedor from "./page/RegistroEmprendedor";
-import DetalleEmprendedor from "./page/DetalleEmprendedor";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  AuthProvider  from './Context/AuthContext';
+import Navbar from './Components/Navbar';
+import Home from './Page/Home';
+import Login from './Page/Login';
+import RegistroEmprendedor from './Page/RegistroEmprendedor';
+import DetalleEmprendedor from './Page/DetalleEmprendedor';
 
 function App() {
     return (
+    <AuthProvider>
         <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-
-                <Route path="/login" element={<Login />} />
-
-                <Route
-                    path="/registro"
-                    element={<RegistroEmprendedor />}
-                />
-
-                <Route
-                    path="/emprendedor/:id"
-                    element={<DetalleEmprendedor />}
-                />
+        <Navbar />
+        
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<RegistroEmprendedor />} />
+            <Route path="/emprendedor/:id" element={<DetalleEmprendedor />} />
             </Routes>
         </BrowserRouter>
+        </AuthProvider>
     );
 }
 
--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { type Emprendedor } from "../Types/Emprendedor";
-import CardEmprendedor from "../components/CardEmprendedor";
+import CardEmprendedor from "../Components/CardEmprendedor";
 
 function Home() {
     const [emprendedores, setEmprendedores] = useState<Emprendedor[]>([]);
